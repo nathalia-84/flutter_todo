6c0a61eebe9b7ca0178c6a49d423c08dc2b33508
nathalia <nathaliaazmelo@gmail.com>
2025-12-29 09:08:18 -0300

    feat: Implement task progression and UI enhancements
    - Convert `MyApp` and `Task` from Stateless to Stateful widgets to manage state.
    - Add an `AnimatedOpacity` to the main list view, controlled by a FloatingActionButton that toggles visibility.
    - Enhance the `Task` widget to include an image, a difficulty rating (represented by stars), and a level.
    - Implement a level-up system for each task, where pressing an "Up" button increments the task's level.
    - Add a `LinearProgressIndicator` to each task to visually represent the current level relative to the task's difficulty.
    - Update the overall UI with more detailed styling, including rounded corners and better layout for task details.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -16,70 +23,174 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter layout demo'),
+          leading: Container(),
+          title: const Text('Tarefas'),
         ),
-        body: ListView(children: const [
-          Task('Aprender Flutter'),
-          Task('Praticar Natação'),
-          Task('Praticar Yoga'),
-          Task('Aprender a andar de bike'),
-          Task('Aprender Flutter'),
-          Task('Praticar Natação'),
-          Task('Praticar Yoga'),
-          Task('Aprender a andar de bike'),
-        ]),
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: const Duration(milliseconds: 300),
+          child: ListView(
+            children: [
+              Task(
+                  'Aprender Flutter',
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                  3),
+              Task(
+                  'Andar de Bike',
+                  'https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                  2),
+              Task(
+                  'Meditar',
+                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                  5),
+              Task(
+                  'Ler',
+                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                  4),
+              Task('Jogar',
+                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 1),
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {}, child: const Icon(Icons.add)
-        ),
+            onPressed: () {
+              setState(() {
+                opacidade = !opacidade;
+              });
+            },
+            child: const Icon(Icons.visibility)),
       ),
     );
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
+  final String foto;
+  final int dificuldade;
 
-  const Task(this.nome, {super.key});
+  const Task(this.nome, this.foto, this.dificuldade, {Key? key})
+      : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 1;
+
+  void levelUp() {
+    setState(() {
+      nivel++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    int nivel = 0;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Stack(
         children: [
           Container(
-            color: Colors.blue,
             height: 140,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: Colors.blue,
+            ),
           ),
           Column(
             children: [
               Container(
-                color: Colors.white,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
+                ),
                 height: 100,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
-                      color: Colors.black26,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.black26,
+                      ),
                       width: 72,
                       height: 100,
-                    ),
-                    Container(
-                      width: 200,
-                      height: 100,
-                      child: Text(
-                        nome,
-                        style: const TextStyle(fontSize: 24),
-                        overflow: TextOverflow.ellipsis,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(4),
+                        child: Image.network(
+                          widget.foto,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                    ElevatedButton(
-                        onPressed: () {}, child: const Icon(Icons.arrow_drop_up)),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: const TextStyle(fontSize: 24),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Row(
+                          children: List.generate(5, (index) {
+                            return Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.dificuldade > index)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            );
+                          }),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 52,
+                      height: 52,
+                      child: ElevatedButton(
+                          onPressed: levelUp,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              const Icon(Icons.arrow_drop_up),
+                              Text(
+                                'Up',
+                                style: TextStyle(fontSize: 12),
+                              )
+                            ],
+                          )),
+                    ),
                   ],
                 ),
               ),
-              Text('Nivel $nivel', style: TextStyle(color: Colors.white, fontSize: 16),)
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        value: widget.dificuldade > 0
+                            ? (nivel / widget.dificuldade) / 10
+                            : 1,
+                        color: Colors.white,
+                        minHeight: 5,
+                      ),
+                    ),
+                    Text(
+                      'Nivel $nivel',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
             ],
           )
         ],
